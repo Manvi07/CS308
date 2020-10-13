3f21fa91cc707f14f80563e1612f638a8452f047
Manvi07 <b17092@students.iitmandi.ac.in>
2020-10-13 21:23:51 +0530

    Changed in master

--- Lab2/sqrt.c
+++ Lab2/sqrt.c
@@ -10,7 +10,7 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("Usage: ./sqrt.o input\n\n");
 		exit(-1);
 	}
 
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	else
 	printf("Sqrt of %d is %fi\n",input,sqrt(-input));
-	printf("End of program. Exiting.\n");
+	printf("End of program. Bye!.\n");
 	return(0);
 
 } // end main
